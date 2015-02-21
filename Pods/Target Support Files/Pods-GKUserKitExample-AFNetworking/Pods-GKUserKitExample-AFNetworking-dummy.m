18c35ee22579285ec1f5483678cc06883410352c
goku <kakarrot.sh@qq.com>
2015-02-21 20:28:44 +0800

    清除文件

--- Pods/Target Support Files/Pods-GKUserKitExample-AFNetworking/Pods-GKUserKitExample-AFNetworking-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GKUserKitExample_AFNetworking : NSObject
-@end
-@implementation PodsDummy_Pods_GKUserKitExample_AFNetworking
-@end
--- Pods/Target Support Files/Pods-GKUserKitExample/Pods-GKUserKitExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GKUserKitExample : NSObject
-@end
-@implementation PodsDummy_Pods_GKUserKitExample
-@end
