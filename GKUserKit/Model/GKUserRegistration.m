1c3c0bb6db9823bd208ecb14328b8c24e5bc6b40
goku <kakarrot.sh@qq.com>
2015-02-23 23:28:52 +0800

    新的注册接口

---
+++ GKUserKit/Model/GKUserRegistration.m
@@ -0,0 +1,13 @@
+//
+//  GKUserRegistration.m
+//  GKUserKitExample
+//
+//  Created by 小悟空 on 2/23/15.
+//  Copyright (c) 2015 GKCommerce. All rights reserved.
+//
+
+#import "GKUserRegistration.h"
+
+@implementation GKUserRegistration
+
+@end
