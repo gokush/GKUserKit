2367c04e6a41f52db53c9fe83d469199f6a93afe
knight731 <knight731@hotmail.com>
2015-02-24 22:47:54 +0800

    Mock

--- GKUserKit/Model/GKUserBackend/GKUserBackendMock.m
+++ GKUserKit/Model/GKUserBackend/GKUserBackendMock.m
@@ -9,7 +9,8 @@
 #import "GKUserBackendMock.h"
 #import <ReactiveCocoa/ReactiveCocoa.h>
 #import "GKUserAccessToken.h"
-
+#import "GKUser.h"
+#import "GKUser"
 @implementation GKUserBackendMock
 
 - (RACSignal *) submitUserFormData:(NSString *)email passWord:(NSString *)passWord {
@@ -22,6 +23,17 @@
         
         [subscriber sendNext:accessToken];
         [subscriber sendCompleted];
+        
+        return [RACDisposable disposableWithBlock:^{
+        }];
+    }];
+}
+- (RACSignal *)signup:(GKUserRegistration *)user
+{
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        GKUser *user;
+        GKUserQueue queue;
+        [subscriber sendNext:RACTuplePack(user,queue)];
         
         return [RACDisposable disposableWithBlock:^{
         }];
