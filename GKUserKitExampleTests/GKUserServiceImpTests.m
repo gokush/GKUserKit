c9c609052821d13f69a489b85d0b7f270afd5f99
童小波 <t.xiaobo.90@gmail.com>
2015-02-26 21:29:37 +0800

    serveice test

---
+++ GKUserKitExampleTests/GKUserServiceImpTests.m
@@ -0,0 +1,40 @@
+//
+//  GKUserServiceImpTests.m
+//  GKUserKitExample
+//
+//  Created by 童小波 on 15/2/26.
+//  Copyright (c) 2015年 GKCommerce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface GKUserServiceImpTests : XCTestCase
+
+@end
+
+@implementation GKUserServiceImpTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testSignup
+{
+    
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
